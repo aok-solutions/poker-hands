0c2cccb1736ed46b239bb7bf0d344fee5a99c31f
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2022-09-03 21:46:45 -0500

    card to select name-that-hand game

--- screens/GamesScreen.tsx
+++ screens/GamesScreen.tsx
@@ -1,11 +1,25 @@
 import { StyleSheet } from 'react-native';
+import { AppleCard } from "react-native-apple-card-views"
 
 import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import nameThatHandImage from '../assets/images/name-that-hand.png'
 
 export default function GamesScreen({ navigation }: RootTabScreenProps<'Games'>) {
   return (
     <View style={styles.container}>
+      <AppleCard
+        smallTitle=""
+        largeTitle="Name That Hand"
+        largeTitleTextStyle={{ fontSize: 50 }}
+        backgroundStyle={{
+          height: 300,
+        }}
+        footnoteText="Multiple choices, so pick the right one!"
+        resizeMode="cover"
+        source={nameThatHandImage}
+        onPress={() => {}}
+      />
     </View>
   );
 }
@@ -14,7 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#fff',
+    paddingVertical: 50
   },
 });
