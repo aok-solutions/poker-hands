7f4653fbc846f4ca3fa3f339807ad077120045a0
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2022-09-07 00:30:29 -0500

    straight logic

--- components/PokerHand.tsx
+++ components/PokerHand.tsx
@@ -1,4 +1,4 @@
-import { Card, Rank, Suit } from "Card"
+import { Card, Rank } from "Card"
 
 export enum Hand {
   HighCard,
@@ -16,17 +16,17 @@ export enum Hand {
 export const getHands = (cards: Card[]): Hand[] => {
   let hands: Hand[] = [Hand.HighCard]
 
-  let ranks = new Map<Rank, number>([])
+  let rankFrequencies = new Map<Rank, number>([])
   cards.forEach((card) => {
-    if (ranks.has(card.props.rank)) {
-      let count = ranks.get(card.props.rank) || 1
-      ranks.set(card.props.rank, count + 1)
+    if (rankFrequencies.has(card.props.rank)) {
+      let count = rankFrequencies.get(card.props.rank) || 1
+      rankFrequencies.set(card.props.rank, count + 1)
     } else {
-      ranks.set(card.props.rank, 1)
+      rankFrequencies.set(card.props.rank, 1)
     }
   })
 
-  const rankCounts: number[] = Array.from(ranks.values())
+  const rankCounts: number[] = Array.from(rankFrequencies.values())
   if (rankCounts.includes(4)) hands.push(Hand.FourOfAKind)
   if (rankCounts.includes(3) && rankCounts.includes(2)) hands.push(Hand.FullHouse)
   if (rankCounts.includes(3)) hands.push(Hand.ThreeOfAKind)
@@ -37,6 +37,18 @@ export const getHands = (cards: Card[]): Hand[] => {
 
   const isFlush = cards.filter((card) => card.props.suit === cards[0].props.suit).length === 5
   if (isFlush) hands.push(Hand.Flush)
+
+  const ranks: Rank[] = cards.map((c) => c.props.rank).sort((a: Rank, b: Rank) => a - b)
+  const straight = [...Array(5).keys()].map((i) => i + ranks[0])
+  const isStraight = ranks.map((rank, index) => rank === straight[index]).reduce((a, b) => a && b)
+  if (isStraight) {
+    if (isFlush) {
+      const hasAceHigh = ranks[ranks.length - 1] === 12 // Rank.Ace is 12
+      if (hasAceHigh) hands.push(Hand.RoyalFlush)
+      hands.push(Hand.StraightFlush)
+    }
+    hands.push(Hand.Straight)
+  }
 
   return hands
 }
--- components/__tests__/PokerHand.test.tsx
+++ components/__tests__/PokerHand.test.tsx
@@ -42,19 +42,19 @@ describe("PokerHand.getHands", () => {
     it("returns a three of a kind", () => {
       expect(getHands(cards)).toContain(Hand.ThreeOfAKind)
     })
-  })
 
-  describe("with three cards and two cards of the same rank", () => {
-    let cards = [
-      <Card rank={Rank.Ace} suit={Suit.Clubs} />,
-      <Card rank={Rank.Ace} suit={Suit.Diamonds} />,
-      <Card rank={Rank.King} suit={Suit.Hearts} />,
-      <Card rank={Rank.King} suit={Suit.Spades} />,
-      <Card rank={Rank.King} suit={Suit.Clubs} />
-    ]
+    describe("with another two cards of the same rank", () => {
+      let cards = [
+        <Card rank={Rank.Ace} suit={Suit.Clubs} />,
+        <Card rank={Rank.Ace} suit={Suit.Diamonds} />,
+        <Card rank={Rank.King} suit={Suit.Hearts} />,
+        <Card rank={Rank.King} suit={Suit.Spades} />,
+        <Card rank={Rank.King} suit={Suit.Clubs} />
+      ]
 
-    it("returns a full house", () => {
-      expect(getHands(cards)).toContain(Hand.FullHouse)
+      it("returns a full house", () => {
+        expect(getHands(cards)).toContain(Hand.FullHouse)
+      })
     })
   })
 
@@ -83,6 +83,48 @@ describe("PokerHand.getHands", () => {
 
     it("returns a flush", () => {
       expect(getHands(cards)).toContain(Hand.Flush)
+    })
+  })
+
+  describe("with five sequentially ranking cards", () => {
+    let cards = [
+      <Card rank={Rank.Queen} suit={Suit.Clubs} />,
+      <Card rank={Rank.Jack} suit={Suit.Hearts} />,
+      <Card rank={Rank.Ten} suit={Suit.Diamonds} />,
+      <Card rank={Rank.Nine} suit={Suit.Spades} />,
+      <Card rank={Rank.Eight} suit={Suit.Clubs} />
+    ]
+
+    it("returns a straight", () => {
+      expect(getHands(cards)).toContain(Hand.Straight)
+    })
+
+    describe("with the same suit", () => {
+      let cards = [
+        <Card rank={Rank.Queen} suit={Suit.Spades} />,
+        <Card rank={Rank.Jack} suit={Suit.Spades} />,
+        <Card rank={Rank.Ten} suit={Suit.Spades} />,
+        <Card rank={Rank.Nine} suit={Suit.Spades} />,
+        <Card rank={Rank.Eight} suit={Suit.Spades} />
+      ]
+
+      it("returns a straight flush", () => {
+        expect(getHands(cards)).toContain(Hand.StraightFlush)
+      })
+
+      describe("with an Ace high", () => {
+        let cards = [
+          <Card rank={Rank.Ace} suit={Suit.Spades} />,
+          <Card rank={Rank.King} suit={Suit.Spades} />,
+          <Card rank={Rank.Queen} suit={Suit.Spades} />,
+          <Card rank={Rank.Jack} suit={Suit.Spades} />,
+          <Card rank={Rank.Ten} suit={Suit.Spades} />
+        ]
+
+        it("returns a royal flush", () => {
+          expect(getHands(cards)).toContain(Hand.RoyalFlush)
+        })
+      })
     })
   })
 })
