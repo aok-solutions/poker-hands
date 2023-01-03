cc030a4c6e62ecbcebb59fcd7d04abb5a9d475da
Ayanga Okpokowuruk <ayanga.okpokowuruk@chime.com>
2023-01-02 21:28:05 -0600

    remove unused components

--- screens/CheatSheetModal.tsx
+++ screens/CheatSheetModal.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar"
-import { Image, Platform, StyleSheet } from "react-native"
+import { Image, Platform } from "react-native"
+import { View } from "react-native-ui-lib"
 
-import { View } from "components/Themed"
 import pokerHands from "assets/images/poker-hands.png"
 
 export default function CheatSheetModal() {
   return (
-    <View style={styles.container}>
+    <View flex backgroundColor="white" center>
       <Image source={pokerHands} style={{ width: 300, height: 650 }} />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
@@ -14,20 +14,3 @@ export default function CheatSheetModal() {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%"
-  }
-})
--- screens/PreferencesScreen.tsx
+++ screens/PreferencesScreen.tsx
@@ -1,15 +1,5 @@
-import { StyleSheet } from "react-native"
-
-import { View } from "components/Themed"
+import { View } from "react-native-ui-lib"
 
 export default function PreferencesScreen() {
-  return <View style={styles.container}></View>
+  return <View flex center backgroundColor="white" />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-})
